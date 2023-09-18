c6c64805afc9acd7e06a19f1bb1d437e7334113e
Oleg <ungunovoleg@gmail.com>
2023-09-18 14:01:23 +0300

    first

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  const data = content.split('\r\n').slice(1)
+  //console.log(data.length-1);
+  const biba = data.map(el => el.split(','))
+  const res2 = biba.map(el => el[1])
+
+  const false1 = res2.filter((el) => el === "FALSE").length
+  const true1 = res2.filter((el) => el === "TRUE").length
+ //console.log(`Рейтинговых игр ${true1}, Не рейтинговых игр ${false1}`)
+ const res4 = biba.map(el => el[13])
+ console.log(_.uniq(res4))
   // END
 }
